2e9b1281fee575835492a01dbbd87918e1e2b220
user <krystian.m342@gmail.com>
2025-04-26 19:36:05 +0200

    krok3

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,21 @@ public class Main {
     public static void main(String[] args) {
 //        Point p1 = new Point(1.0,3.0);
 //        System.out.println(p1.x);
-        List<Point> tri_points = new ArrayList<>(); // tutaj jest new ArrayList bo List to tylko implementacja
-        tri_points.add(new Point(1.0,2.0));
-        tri_points.add(new Point(2.0,3.0));
-        tri_points.add(new Point(3.0,4.0));
-        Polygon triangle = new Polygon(tri_points);
+
+//        List<Point> tri_points = new ArrayList<>(); // tutaj jest new ArrayList bo List to tylko implementacja
+//        tri_points.add(new Point(1.0,2.0));
+//        tri_points.add(new Point(2.0,3.0));
+//        tri_points.add(new Point(3.0,4.0));
+//        Polygon triangle = new Polygon(tri_points);
+
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(0, 0));
+        points.add(new Point(3, 0));
+        points.add(new Point(0, 3));
+        Polygon triangle = new Polygon(points);
+        Point pointIn = new Point(1, 1);
+        System.out.println(triangle.inside(pointIn)); // true
+        Point pointOut = new Point(4, 4);
+        System.out.println(triangle.inside(pointOut)); // false
     }
 }
--- src/Polygon.java
+++ src/Polygon.java
@@ -6,4 +6,31 @@ public class Polygon {
     public Polygon(List<Point> points) {
         this.points = List.copyOf(points);
     }
+
+    public boolean inside(Point point){
+        int counter = 0;
+        int n = points.size();
+        for(int i=0; i<n; i++){
+            Point pa = points.get(i);
+            Point pb = points.get((i + 1) % n);
+            if(pa.y > pb.y){
+                Point temp = pa;
+                pa = pb;
+                pb = temp;
+            }
+            if(pa.y < point.y && point.y < pb.y){
+                double d = pb.x - pa.x;
+                double x;
+                if(d == 0){
+                    x = pa.x;
+                } else{
+                    double a = (pb.y - pa.y) / d;
+                    double b = pa.y - a * pa.x;
+                    x = (point.y - b) / a;
+                }
+                if(x < point.x) counter++;
+            }
+        }
+        return (counter % 2) != 0;
+    }
 }
